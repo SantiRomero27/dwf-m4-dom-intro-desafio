98a3434f28153f81c39c5759096c8de695393ecf
Santiago <romerogherdinasantiago@gmail.com>
2021-06-01 22:31:49 -0300

    Desafío hecho. Revisar por cualquier cosa

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
 const cosasQueAprendimos = [
-  {
-    tema: "terminal",
-    class: "",
-  },
-  {
-    tema: "node",
-    class: "",
-  },
-  {
-    tema: "oop",
-    class: "",
-  },
-  {
-    tema: "typescript",
-    class: "",
-  },
-  {
-    tema: "css",
-    class: "",
-  },
-  {
-    tema: "dom",
-    class: "special",
-  },
+    {
+        tema: "terminal",
+        class: "",
+    },
+    {
+        tema: "node",
+        class: "",
+    },
+    {
+        tema: "oop",
+        class: "",
+    },
+    {
+        tema: "typescript",
+        class: "",
+    },
+    {
+        tema: "css",
+        class: "",
+    },
+    {
+        tema: "dom",
+        class: "special",
+    },
 ];
 
-function main() {}
+// Función para borrar los li dentro de un ul
+function borrarLi() {
+    const liArray = document.getElementsByTagName("li");
 
+    // Borro los li
+    for (let i = liArray.length - 1; i >= 0; i--) {
+        let liEl = liArray[i];
+
+        // Para borrarlos, primero instancio al nodo padre, para después borrar el hijo
+        liEl.parentNode.removeChild(liEl);
+    }
+}
+
+// Función para crear elementos y hacerle el append al ul
+function crearElementos() {
+    // Obtengo la lista
+    const elUl = document.getElementsByClassName("lista")[0];
+
+    // Con un forEach, recorro todos los objetos, y creo los elementos
+    cosasQueAprendimos.forEach((item) => {
+        // Creo el elemento vacío
+        let newEl = document.createElement("li");
+
+        // Le asigno un contenido, y una clase
+        newEl.textContent = item.tema;
+
+        if (item.class != "") {
+            newEl.setAttribute("class", item.class);
+        }
+
+        // Ahora, sólo hago el append
+        elUl.appendChild(newEl);
+    });
+}
+
+// Función principal
+function main() {
+    // Borro todos los li
+    borrarLi();
+
+    // Creo los elementos del array
+    crearElementos();
+}
+
+// EJECUCION
 main();
